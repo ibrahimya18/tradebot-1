791fca6360898f315795f7d460063ae4cae3eda5
ibrahim_samed <IbrahimSamed.Yalcin@vestel.com.tr>
2025-05-07 04:49:30 +0300

    Update Chart.jsx

--- src/components/Chart.jsx
+++ src/components/Chart.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import { Line } from 'react-chartjs-2';
-import { Chart as ChartJS } from 'chart.js/auto';
 
 export const Chart = ({ priceHistory, currentPrice }) => {
   const data = {
